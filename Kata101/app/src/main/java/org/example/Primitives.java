a4ac7726b4752813878b8523510f7ffea57639ad
marcello-s <marcello.s@gmx.ch>
2025-12-08 15:49:54 +0100

    add primitive types example

---
+++ Kata101/app/src/main/java/org/example/Primitives.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Primitives {
+    int intValue;
+    double doubleValue;
+    boolean booleanValue;
+    byte byteValue;
+    char charValue;
+    short shortValue;
+    long longValue;
+    float floatValue;
+    String stringValue = Primitives.class.getSimpleName();
+}
