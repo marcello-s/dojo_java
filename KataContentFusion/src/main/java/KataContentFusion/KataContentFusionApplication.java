0419b77b70c9d620987786604132e5c6810dcfac
marcello-s <marcello.s@gmx.ch>
2026-02-16 22:40:58 +0100

    KataContentFusion setup, SpringBoot, MySQL

---
+++ KataContentFusion/src/main/java/KataContentFusion/KataContentFusionApplication.java
@@ -0,0 +1,13 @@
+package KataContentFusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataContentFusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataContentFusionApplication.class, args);
+	}
+
+}
